0847773dd784d422327ba32ea04a1f86be271a5a
Zirael <igor.www.pl.com@gmail.com>
2023-08-14 19:35:33 +0200

    business wip

--- Portfolio/src/routes/Business.svelte
+++ Portfolio/src/routes/Business.svelte
@@ -1,10 +1,37 @@
 <script>
-    
+    const themes = {
+    'light-default': {
+      background: 'white',
+      borderColor: '#e1e4e8',
+      color: '#586069',
+      linkColor: '#0366d6',
+    },
+    'dark-theme': {
+      background: 'rgb(13, 17, 23)',
+      borderColor: 'rgb(48, 54, 61)',
+      color: 'rgb(139, 148, 158)',
+      linkColor: 'rgb(88, 166, 255)',
+    }
+  };
 </script>
 
 <body style="height: 2137px;">
     <div class="bg">
-        <h1>Business page</h1>
+        <h1>Igor Heliński</h1>
+        <ul class="chapters">
+            <li>About me
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident accusamus officiis eveniet deserunt dicta beatae, minima aliquid iste fugit nostrum quia aperiam soluta maiores, cupiditate dignissimos distinctio. Accusantium, distinctio id!</p>
+            </li>
+            <li>My Experience
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident accusamus officiis eveniet deserunt dicta beatae, minima aliquid iste fugit nostrum quia aperiam soluta maiores, cupiditate dignissimos distinctio. Accusantium, distinctio id!</p>
+            </li>
+            <li>Skills
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident accusamus officiis eveniet deserunt dicta beatae, minima aliquid iste fugit nostrum quia aperiam soluta maiores, cupiditate dignissimos distinctio. Accusantium, distinctio id!</p>
+            </li>
+            <li>Résumé
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae amet tempora modi, consequuntur explicabo quis quasi ex delectus iusto aliquid aliquam? Maiores aliquam ducimus suscipit assumenda animi, hic recusandae quaerat?</p>
+            </li>
+        </ul>
     </div>
 </body>
 
@@ -16,10 +43,30 @@
 
     .bg{
         margin: 50px;
+        padding: 15px;
+        padding-top: 80px;
         background-color: whitesmoke;
+        border-radius: 20px;
     }
 
     h1{
-        color: black;
+        margin-bottom: 20px;
+        color:black;
+        text-align: center;
+        font-size: 55px;
+    }
+
+    .chapters li{
+        margin: auto 110px;
+
+        color:black;
+        font-weight: bold;
+        font-size: x-large;
+    }
+
+    .chapters li p{
+        color:black;
+        font-weight: 400;
+        font-size: large;
     }
 </style>
